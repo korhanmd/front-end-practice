508d916700e725a79b2c09d5cce29c7fd4c21f4d
Korhan Mutludoğan <korhanmd@gmail.com>
2022-08-14 15:14:35 +0300

    Change API library

--- dropdown_menu/server_simulator/script.js
+++ dropdown_menu/server_simulator/script.js
@@ -36,7 +36,7 @@ function getFunction(url, data, callback) {
   const domain = url.substring(0, url.indexOf("/"));
   const endpoint = url.substring(url.indexOf("/"), url.length);
 
-  callback(endpoints[endpoint]["get"]);
+  callback(endpoints[endpoint]["get"][data]);
 }
 
 const api = {
