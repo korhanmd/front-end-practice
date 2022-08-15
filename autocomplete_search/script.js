4073579fc5fcc768cdf31b1014bfc54bc71acb79
Korhan Mutludoğan <korhanmd@gmail.com>
2022-08-15 21:16:44 +0300

    Add randomness to suggestions

--- autocomplete_search/script.js
+++ autocomplete_search/script.js
@@ -15,14 +15,45 @@ searchInput.oninput = onNewInput;
 
 // Server
 
+function getRandomString({length}) { ... }
+function getRandomInteger({min, max}) { ... }
+
+function generateSuggestion(prefix) {
+	const RATIO_EXACT_MATCH = 0.3;
+	const RATIO_AUTOCORRECT = 0.1;
+
+	if (Math.random() < RATIO_AUTOCORRECT) {
+		return getRandomString({ length: getRandomInteger({min: 1, max: prefix.length})});
+	}
+
+	if (Math.random() < RATIO_EXACT_MATCH) {
+		return prefix;
+	}
+
+	return prefix + getRandomString({ length: getRandomInteger({min: 1, max: 10})});
+}
+
 function getAutocompleteHandler(data) {
+	const MAX_CHARS = 10;
 	const NUM_AUTOCOMPLETE_RESULTS = 10;
+	const RATIO_AUXILIARY_DATA = 0.1;
+
+	if (data.length > MAX_CHARS) {
+		return [];
+	}
+
 	const results = [];
-	for (let i = 0; i < NUM_AUTOCOMPLETE_RESULTS; i++) {
-		results.push({
-			suggestion: data + "asdf",
-			auxiliary: "asdf"
-		});
+	while (results.length < NUM_AUTOCOMPLETE_RESULTS) {
+		const suggestion = generateSuggestion(data);
+
+		if (Math.random() < RATIO_AUXILIARY_DATA) {
+			results.push({
+				suggestion,
+				auxiliary: getRandomString({length: getRandomInteger({min: 5, max: 15})})
+			});
+		} else {
+			results.push({suggestion, auxiliary: ""});
+		}
 	}
 	return results;
 }
