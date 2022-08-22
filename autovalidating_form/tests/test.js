59ff5e53832408d459acf11d7d7485172bcdaff7
Korhan Mutludoğan <korhanmd@gmail.com>
2022-08-22 15:30:59 +0300

    Write example unit tests

---
+++ autovalidating_form/tests/test.js
@@ -0,0 +1,65 @@
+function isValidName(name) {
+	return;
+}
+
+function isValidEmail(email) {
+	return;
+}
+
+// Unit Tests
+
+function runTests() {
+	firstNameTest();
+	emailTest();
+}
+
+runTests();
+
+function runner({inputs, expectedOutputs, func}) {
+	let results = "";
+	for (let i = 0; i < inputs.length; i++) {
+		const passFailString = func(inputs[i]) === expectedOutputs[i] ?
+			'Pass' :
+			'<span style="color: red">Fail</span>';
+
+		const result =  `${func.name}(${inputs[i]}) === ${expectedOutputs[i]}: ${passFailString}`;
+		results += (result + "<br>");
+	}
+
+	const resultsElement = document.getElementsByClassName('results')[0];
+	resultsElement.innerHTML += (results + "<br>");
+}
+
+function firstNameTest() {
+	const invalidInputs = ["@", "", "blah$", "123"];
+	const validInputs = ["asdf", "Alfred", "ALFRED"];
+
+	runner({
+		inputs: validInputs,
+		expectedOutputs: validInputs.map(_ => true),
+		func: isValidName
+	});
+
+	runner({
+		inputs: invalidInputs,
+		expectedOutputs: invalidInputs.map(_ => false),
+		func: isValidName
+	});
+}
+
+function emailTest() {
+	const invalidEmails = ["@asdf.com", "what@what", "", ".."];
+	const validEmails = ["asdf@asdf.com", "what@what.au", "a@a.c"];
+
+	runner({
+		inputs: validEmails,
+		expectedOutputs: validEmails.map(_ => true),
+		func: isValidEmail
+	});
+
+	runner({
+		inputs: invalidEmails,
+		expectedOutputs: invalidEmails.map(_ => false),
+		func: isValidEmail
+	});
+}
