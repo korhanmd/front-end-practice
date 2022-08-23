732b250503b4da07d70d40de2008deb344a7e1c0
Korhan Mutludoğan <korhanmd@gmail.com>
2022-08-23 17:12:45 +0300

    Display actual message for name

--- autovalidating_form/script.js
+++ autovalidating_form/script.js
@@ -1,6 +1,15 @@
+class ValidationError extends Error {
+    constructor(message) {
+        super();
+        this.message = message;
+    }
+}
+
 function validateName(name) {
     const nameRegex = /^[a-zA-Z]+$/;
-    return nameRegex.test(name);
+    if (!nameRegex.test(name)) {
+        throw new ValidationError('Please enter a valid name');
+    }
 }
 
 function validateUsername(username) {
@@ -25,12 +34,25 @@ function validateEmail(email) {
 
 function validate(event) {
     const inputElement = event.target;
-    inputElement.classList.add('signup__field__input--error');
 
     const field = inputElement.dataset.field;
 
     const errorMessageElement = event.target.parentElement.getElementsByClassName('signup__field__error')[0];
-    errorMessageElement.innerHTML = `Error for ${field}`;
+
+    try {
+        validateName(inputElement.value);
+        errorMessageElement.innerHTML = '';
+        inputElement.classList.remove('signup__field__input--error');
+    } catch (err) {
+        if (!(err instanceof ValidationError)) {
+            // Log real error
+            throw err;
+        }
+        
+        errorMessageElement.innerHTML = err.message;
+        inputElement.classList.add('signup__field__input--error');
+    }
+
 }
 
 const inputs = document.getElementsByClassName('signup__field__input');
