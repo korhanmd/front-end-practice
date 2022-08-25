4847231f7d2d18e26f0d73c1c44eabd32904109f
Korhan Mutludoğan <korhanmd@gmail.com>
2022-08-25 15:51:13 +0300

    Implement handlers

--- infinite_scroll/server.js
+++ infinite_scroll/server.js
@@ -30,6 +30,24 @@ class Database {
 	}
 }
 
+const database = new Database();
+
+function getTweetsHandler(data) {
+	const pageSize = data.pageSize;
+	const sortOrder = data.sortOrder;
+	const lastTweetId = data.lastTweetId;
+
+	if (sortOrder !== 'recent') {
+		throw new Error('I dont know how to handle that');
+	}
+
+	return database.query({lastTweetId, pageSize});
+}
+
+function postTweetHandler(data) {
+	database.insert(data.tweet);
+}
+
 const endpoints = {
 	"/tweets": {
 		"get": getTweetsHandler,
