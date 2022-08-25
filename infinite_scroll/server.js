824975b2a7094474f5d9acb05284114c243aa6a3
Korhan Mutludoğan <korhanmd@gmail.com>
2022-08-25 15:40:35 +0300

    Use random string and integer functions from the autocomplete search project

--- infinite_scroll/server.js
+++ infinite_scroll/server.js
@@ -1,2 +1,17 @@
 // Server
 
+function getRandomString({length}) {
+	const characterChoices = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 ";
+	const characters = [];
+
+	while (characters.length < length) {
+		const randomIndex = Math.floor(Math.random() * characterChoices.length);
+		characters.push(characterChoices[randomIndex]);
+	}
+
+	return characters.join('');
+}
+
+function getRandomInteger({min, max}) {
+	return Math.floor(Math.random() * (max - min) + min);
+}
